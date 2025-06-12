5d117d4a058299cceac8e51636599670bee929b5
Sebastian Villalba <sebas-juancho2003@hotmail.com>
2025-06-12 07:27:34 -0500

    Uso de Polygon para nombre

--- Instrumentos-Financieros-/Componentes/app/sources/market_client.py
+++ Instrumentos-Financieros-/Componentes/app/sources/market_client.py
@@ -4,7 +4,8 @@ from datetime import datetime, timedelta
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
-import yfinance as yf
+
+from polygon import RESTClient
 
 from dotenv import load_dotenv
 
@@ -12,10 +13,12 @@ load_dotenv()
 
 class MarketClient:
     def __init__(self):
-        self.client = StockHistoricalDataClient(
+        self.alpaca = StockHistoricalDataClient(
             os.getenv("ALPACA_API_KEY"),
             os.getenv("ALPACA_SECRET_KEY")
         )
+
+        self.polygon = RESTClient(os.getenv("POLYGON_API_KEY"))
     
     def get_daily_change(self, symbols: list[str]):
         hoy = datetime.now().date()
@@ -28,7 +31,8 @@ class MarketClient:
             end=hoy
         )
 
-        bars = self.client.get_stock_bars(params).df        
+        bars = self.alpaca.get_stock_bars(params).df 
+         
         resultados = []        
         if bars.empty:
             return resultados  # Lista vacía si no hay datos
@@ -36,20 +40,19 @@ class MarketClient:
         # Agrupar por símbolo usando el índice de nivel 0
         for symbol, df_symbol in bars.groupby(level=0):
             if len(df_symbol) < 2:
-                continue  # Ignora si no hay suficientes datos
+                continue  # Ignora si no hay suficientes datos            
 
             ultimo = df_symbol.iloc[-1]["close"]
             anterior = df_symbol.iloc[-2]["close"]
             variacion = ((ultimo - anterior) / anterior) * 100
 
-            stock = yf.Ticker(symbol)
-            nombre = stock.info.get("shortName")
-
+            details = self.polygon.get_ticker_details(symbol)
+            companyName = details.name
             tokenLogo = os.getenv("LOGO_API_KEY")
 
             resultados.append({
                 "icono": f"https://img.logo.dev/ticker/{symbol}?token={tokenLogo}",
-                "titulo": nombre,
+                "titulo": companyName,
                 "ticker": symbol,
                 "valor": round(ultimo, 2),
                 "diferencia": round(variacion, 2)
